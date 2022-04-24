8ae13ed751bb3ad11ff2a828d39fdc169522c16e
busisatya@gmail.com <busisatya@gmail.com>
2022-04-23 21:37:11 -0400

    CHG: Login and Signup

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
 import sqlite3
+from flask import Flask, render_template
 
-from flask import Flask,render_template,url_for
-from flask_mysqldb import MySQL
+# Bootstrap for the app
+from flask_bootstrap import Bootstrap
+
+# For Using in signup and logins
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
+
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pds_project'
-my_sql = MySQL(app)
+app.config['SECRET_KEY'] = 'SecretKey'
+Bootstrap(app)
 
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+# This is for showing in login page
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
+class SignupForm(FlaskForm):
+    pass
 
 
 # This if for the landing page
 @app.route('/')
 def hello_world():
-    connection = get_db_connection()
-    posts = connection.execute('SELECT * from test').fetchall()
-    connection.close()
-    return render_template('index.html', data=posts)
-#     It knows to look at templates
+    return render_template('index.html')
+#     It knows where to find the templates
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
